e0b626c129ead86386f44623ea062d33a5056d95
peppapig1911 <158774869+peppapig1911@users.noreply.github.com>
2024-03-18 08:05:35 +0000

    Initial commit

---
+++ fichier.h
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Point{
+
+private :
+
+  int x,y;
+
+public : 
+  void translater(Point p);
+  //constructeur initialise un point à l'origine 
+  Point();
+  //constructeur prenant 2 paramètres réels
+  Point(int a, int b);
+  //
+};
